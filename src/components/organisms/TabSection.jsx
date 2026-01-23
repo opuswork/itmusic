67863eb6a97225085bdbea4b53ca4bf6f7426837
Taeyoung <tseon@opuscore.net>
2026-01-23 18:11:49 +0900

    feat(TabSection) updated image and content location and size

--- src/components/organisms/TabSection.jsx
+++ src/components/organisms/TabSection.jsx
@@ -336,8 +336,8 @@ function ConcertCompetitionBox() {
               <Image
                 src={imageUrl}
                 alt={currentConcert.subject || '공연소식'}
-                width={150}
-                height={200}
+                width={200}
+                height={280}
                 style={{ objectFit: 'cover' }}
                 unoptimized
               />
@@ -399,8 +399,8 @@ function ConcertCompetitionBox() {
               <Image
                 src={imageUrl}
                 alt={currentCompetition.subject || '콩쿠르'}
-                width={150}
-                height={200}
+                width={200}
+                height={280}
                 style={{ objectFit: 'cover' }}
                 unoptimized
               />
