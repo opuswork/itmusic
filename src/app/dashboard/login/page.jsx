4b5018966353ad001ff5be8792641c9b378c6e21
Taeyoung <tseon@opuscore.net>
2026-02-10 15:27:38 +0900

    feat(admin login), added src/app/dashboard/login/page.jsx page.module.css

--- src/app/dashboard/login/page.jsx
+++ src/app/dashboard/login/page.jsx
@@ -1,11 +1,50 @@
 "use client";
 
-import AdminLogin from "./adminLogin";
+import styles from "./page.module.css";
 
 export default function LoginPage() {
   return (
     <>
-      <AdminLogin />
+      <div className={styles.loginBox}>
+          {/* <!-- /.login-logo --> */}
+          <div className={styles.card}>
+              <div className={styles.cardHeader}>
+              <h1 className={styles.h1}><b>Admin Login</b></h1>
+              </div>
+              <div className={styles.cardBody}>
+              <p className={styles.loginBoxMsg}>Please login to start your session</p>
+
+              <form action="/api/auth/login" method="post">
+                  <div className={styles.inputGroup}>
+                  <input type="text" name="id" className={styles.formControl} placeholder="Username" required autoComplete="username" />
+                  <div className={styles.inputGroupAppend}>
+                      <div className={styles.inputGroupText}>
+                      <span className={styles.fasFaEnvelope}></span>
+                      </div>
+                  </div>
+                  </div>
+                  <div className={styles.inputGroup}>
+                  <input type="password" name="password" className={styles.formControl} placeholder="Password" required autoComplete="current-password" />
+                  <div className={styles.inputGroupAppend}>
+                      <div className={styles.inputGroupText}>
+                      <span className={styles.fasFaLock}></span>
+                      </div>
+                  </div>
+                  </div>
+                  <div className={styles.row}>
+
+                  {/* <!-- /.col --> */}
+                  <div className={styles.col4}>
+                      <button type="submit" className={styles.btnPrimaryBtnBlock}>Sign In</button>
+                  </div>
+                  {/* <!-- /.col --> */}
+                  </div>
+              </form>
+
+              {/* <!-- /.social-auth-links --> */}
+              </div>
+          </div>
+      </div>
     </>
   );
 }
